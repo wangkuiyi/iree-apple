c4bd7f55b327315ab162868e4d1c4c320bd60323
Yi Wang <yi.wang.2005@gmail.com>
2023-04-25 12:15:32 -0700

    Make install.sh easier (#11)

--- install.sh
+++ install.sh
@@ -1,12 +1,34 @@
-SCRIPT_DIR=$(realpath $(dirname "$(readlink -f "${BASH_SOURCE[0]}")"))
+#!/bin/bash
+
+SCRIPT_DIR="$(realpath "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 
 IREE_BUILD_DIR=$SCRIPT_DIR/build
 IREE_BUILD_COMPILER_DIR=$IREE_BUILD_DIR/compiler
 IREE_BUILD_COMPILER_BIN_DIR=$IREE_BUILD_COMPILER_DIR/install/bin
 IREE_BUILD_COMPILER_LIB_DIR=$IREE_BUILD_COMPILER_DIR/lib
 
-DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$IREE_BUILD_COMPILER_LIB_DIR
+export DYLD_LIBRARY_PATH=$IREE_BUILD_COMPILER_LIB_DIR:$DYLD_LIBRARY_PATH
+echo "Set DYLD_LIBRARY_PATH to $DYLD_LIBRARY_PATH"
 
-PATH=$PATH:$IREE_BUILD_COMPILER_BIN_DIR
+export PATH=$IREE_BUILD_COMPILER_BIN_DIR:$PATH
+echo "Set PATH to $PATH"
 
-source $IREE_BUILD_COMPILER_DIR/.env
+if iree-compile -h > /dev/null; then
+    echo "iree-compile works"
+else
+    echo "error: iree-compile does not work"
+    exit 2
+fi
+
+IREE_BUILD_COMPILER_PYTHON_BINDING_COMPILER=$IREE_BUILD_COMPILER_DIR/compiler/bindings/python
+IREE_BUILD_COMPILER_PYTHON_BINDING_RUNTIME=$IREE_BUILD_COMPILER_DIR/runtime/bindings/python
+export PYTHONPATH=$IREE_BUILD_COMPILER_PYTHON_BINDING_COMPILER:$IREE_BUILD_COMPILER_PYTHON_BINDING_RUNTIME:$PYTHONPATH
+echo "Set PYTHONPATH to $PYTHONPATH"
+
+IMPORT_TEST="import iree.compiler, iree.runtime"
+if python -c "$IMPORT_TEST"; then
+    echo "$IMPORT_TEST works"
+else
+    echo "error: $IMPORT_TEST does not work"
+    exit 2
+fi
